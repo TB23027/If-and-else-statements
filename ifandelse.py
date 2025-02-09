39443bbc120ce316cde89787094413dc3d33b562
Torr Beaman <tb23027@my.westlake.school.nz>
2025-02-10 12:47:54 +1300

    Finished first task

---
+++ ifandelse.py
@@ -0,0 +1,6 @@
+secret = ("James Bond")
+guess = input("Guess what the secret name is if you wish to enter:")
+if guess == secret:
+    print("You may enter")
+else:
+    print("Remove thy self from these premises")
